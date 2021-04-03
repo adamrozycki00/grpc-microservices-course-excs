19512cb381900926e6de6808a2f0be05cd704a66
adamrozycki00 <adamrozycki00@gmail.com>
2021-04-03 20:29:10 +0200

    Refactor into modules

--- protobuf/src/main/java/com/tenetmind/person/PersonDemo.java
+++ protobuf/src/main/java/com/tenetmind/person/PersonDemo.java
@@ -11,12 +11,13 @@ import java.nio.file.Paths;
 public class PersonDemo {
 
     public static void main(String[] args) throws IOException {
+
         Person sam = Person.newBuilder()
                 .setName("Sam")
                 .setAge(Int32Value.newBuilder().setValue(10).build())
                 .build();
 
-        Path samFile = Paths.get("sam.serial");
+        Path samFile = Paths.get("protobuf/src/main/resources/sam.serial");
         Files.write(samFile, sam.toByteArray());
 
         Person retrievedSam = Person.parseFrom(Files.readAllBytes(samFile));
